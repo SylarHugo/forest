81c8e5d9fc5244ee8ae6030b362db616bddca90a
SylarHugo <40252321+SylarHugo@users.noreply.github.com>
2022-02-23 00:43:20 +0800

    Add files via upload

---
+++ spatialstructure.py
@@ -0,0 +1,23 @@
+class spatial:
+    def __init__(self, x, y, species, DBM, crown):
+        self.x = x
+        self.y = y
+        self.species = species
+        self.DBM = DBM
+        self.crown = crown
+def uniformangle(goal, n):#uniform angle index, observed angle a, standard angle a0=90
+    W = 0
+    return W
+    pass
+def mingling(goal, n):#mingling degree, diffrernt tree species
+    M = 0
+    return M
+    pass
+def neighborhood(goal, n):#neighborhood comparison, DBM
+    U = 0
+    return U
+    pass
+def crowding(goal, n):#crowding degree. basal area, crown area
+    C = 0
+    return C
+    pass
